1e66f5c6cf39fcba34b78597fd55a8e33be07de3
youngtuck <tuckerdhoward@gmail.com>
2026-03-22 20:16:02 -0700

    Update ExplorePage.tsx

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -114,7 +114,7 @@ const CSS = `
   background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)'/%3E%3C/svg%3E");
   opacity: 0.018;
   pointer-events: none;
-  z-index: 9999;
+  z-index: 1;
 }
 
 .xp em { font-style: normal; color: var(--gold); }
